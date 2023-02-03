0f4a7b595e3669e3b015df9c65ec20d840b9d790
PedroRibeir0 <112427753+PedroRibeir0@users.noreply.github.com>
2023-02-03 18:22:03 -0300

    mobile

---
+++ Avant/js/main.js
@@ -0,0 +1,16 @@
+//menu
+let menuHamburger = document.querySelector('#hamburger')
+let closeButton = document.querySelector('#close')
+let homeMenu = document.querySelector('#home-menu')
+menuHamburger.addEventListener('click', ()=>{
+    homeMenu.style.display = 'block'
+    homeMenu.style.animation = 'menuJump 0.6s'
+
+})
+closeButton.addEventListener('click', ()=>{
+    homeMenu.style.animation = 'none'
+    homeMenu.style.display = 'none'
+})
+
+
+
