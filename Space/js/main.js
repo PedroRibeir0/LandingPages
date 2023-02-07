35435e18e162bd704f41fc966a3c84a5843f2ed8
PedroRibeir0 <112427753+PedroRibeir0@users.noreply.github.com>
2023-02-07 14:52:16 -0300

    space ready

--- Space/js/main.js
+++ Space/js/main.js
@@ -5,8 +5,8 @@ let homeMenu = document.querySelector('#home-menu')
 menuHamburger.addEventListener('click', ()=>{
     homeMenu.style.display = 'block'
     homeMenu.style.animation = 'menuJump 0.6s'
-
 })
+
 closeButton.addEventListener('click', ()=>{
     homeMenu.style.animation = 'none'
     homeMenu.style.display = 'none'
